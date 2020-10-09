b2795f0ea7f741c280356d2e4959daca02161ed3
Joao Pedro Prado Leal <joaopedro0128@hotmail.com>
2020-10-09 01:15:06 -0300

    Updates

---
+++ precache-manifest.64821156fbd1edcbe683f9238652288b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2018fbdcb039716ef2e82868a2baea55",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c018cf4aee96abed7c00",
+    "url": "/static/css/2.1c9ef103.chunk.css"
+  },
+  {
+    "revision": "c018cf4aee96abed7c00",
+    "url": "/static/js/2.e90a4ded.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.e90a4ded.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b546ad5857244f3c301",
+    "url": "/static/js/main.388c97a6.chunk.js"
+  },
+  {
+    "revision": "0f01680d2d7c77684457",
+    "url": "/static/js/runtime-main.5bf89cad.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
